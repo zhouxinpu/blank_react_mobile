353b052cd06c718603266182f228eefd962c604e
zhouxinpu <663103104@qq.com>
2019-08-29 14:57:23 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './assets/less/reset.less'
+import { Provider } from 'react-redux'
+import store from './store'
+import User from './views/User'
+import Goods from './views/Goods'
+import {BrowserRouter as Router,Switch,Route,Redirect} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route path="/user" component={User} />
+        <Route path="/goods" component={Goods} />
+        <Redirect to="/user" />
+      </Switch>
+    </Router>
+  </Provider>, document.querySelector('#root'))
